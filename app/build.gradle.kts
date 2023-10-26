0dfd9dc9dcce510cac7fb0f621e0b809a0c03ccb
Eric Smith <essmith20@students.desu.edu>
2023-10-26 17:21:06 -0400

    Proper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,11 +76,14 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0") // Replace x.x with the latest version number
     implementation ("com.loopj.android:android-async-http:1.4.11")
-    implementation ("com.github.bumptech.glide:glide:4.16.0") // replace x.x with the latest version
-    implementation ("com.loopj.android:android-async-http:1.4.9")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
+
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation ("com.google.android.material:material:1.5.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.loopj.android:android-async-http:1.4.11")
+
 
 
 }
